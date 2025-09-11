fd836e040aa4b05742f28b91462ad4d9b719a706
Hema <Hema@DESKTOP-B2OA9R3>
2025-09-11 16:42:28 +0530

    Type Convertions

---
+++ TNS_Java_Project/src/TNS_Java_Package/TypeConvertions.java
@@ -0,0 +1,14 @@
+package TNS_Java_Package;
+
+public class TypeConvertions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float inp = a;
+		byte exp = (byte)a;
+		System.out.println("Implicit conversion: " + inp);
+		System.out.println("Explicit conversion: " + exp);
+	}
+
+}
