a5a7a3ef64290fa918bc07358a728fa14dfe35ce
Hema <Hema@DESKTOP-B2OA9R3>
2025-09-17 17:24:46 +0530

    Manager class in com.suharika.assignment.Employees package

---
+++ TNS_Java_Project/src/com/suharika/assignment/Employees/Manager.java
@@ -0,0 +1,16 @@
+package com.suharika.assignment.Employees;
+
+/**
+ * Manager subclass extending Employee
+ */
+public class Manager extends Employee {
+    private String department;
+
+    public Manager(String name, int employeeId, double salary, String department) {
+        super(name, employeeId, salary);
+        this.department = department;
+    }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
